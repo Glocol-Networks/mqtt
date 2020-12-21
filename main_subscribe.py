99df38778c1874b8fae833ec4bb8d3e5c9c2ccbf
Subhash Reddy <subhash8111999@gmail.com>
2020-12-21 21:49:25 +0530

    Update main_subscribe.py

--- main_subscribe.py
+++ main_subscribe.py
@@ -7,7 +7,9 @@ import pandas as pd
 
 def on_connect(client, userdata, flags, rc):  # The callback for when the client connects to the broker
     print("Connected with result code {0}".format(str(rc)))  # Print result of connection attempt
-    client.subscribe("gpsd_ws/gps","gpsd_ws/extended","gpsd_ws/data")  # Subscribe to the topics , receive any messages published on them
+    client.subscribe("gpsd_ws/gps")
+    client.subscribe("gpsd_ws/extended")
+    client.subscribe("gpsd_ws/data")  # Subscribe to the topics , receive any messages published on them
 
 
 def on_message(client, userdata, msg):  # The callback for when a PUBLISH message is received from the server.
